23bc341f85623e669b9501496fbcba57908e9b67
fkang9002 <fkang9002@163.com>
2017-07-18 19:34:35 +0800

    样式更改2

--- common/config.js
+++ common/config.js
@@ -1,5 +1,20 @@
 
 module.exports = {
   domain: "https://lishifeng2.mywopop.com",
-  website_name: '某某零食'
+  website_name: '某某零食',
+  index_autoplay_img: [
+    '/image/bg-1.jpg',
+    '/image/bg-2.jpg'
+  ],
+  index_middle_img: [
+  {
+      url:'details/details',
+      src: '/image/tj-2.png'
+  },
+  {
+      url: 'details/details',
+      src: '/image/tj-3.png'
+  }],
+  index_middle2_img: '/image/tj-1.jpg'
+  
 }
