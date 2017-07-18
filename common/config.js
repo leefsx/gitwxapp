96b8dcf05371bfcdd673e5edddaeb0eab7d12eb2
leefsx <leefsx@aliyun.com>
2017-07-18 09:32:19 +0800

    改json格式

--- common/config.js
+++ common/config.js
@@ -1,20 +1,20 @@
 
 module.exports = {
-  domain: "https://lishifeng2.mywopop.com",
-  website_name: '某某零食',
-  index_autoplay_img: [
-    '/image/bg-1.jpg',
-    '/image/bg-2.jpg'
+  "domain": "https://lishifeng2.mywopop.com",
+  "website_name": "某某零食",
+  "index_autoplay_img": [
+    "/image/bg-1.jpg",
+    "/image/bg-2.jpg"
   ],
-  index_middle_img: [
+  "index_middle_img": [
   {
-      url:'details/details',
-      src: '/image/tj-2.png'
+      "url":"details/details",
+      "src": "/image/tj-2.png"
   },
   {
-      url: 'details/details',
-      src: '/image/tj-3.png'
+      "url": "details/details",
+      "src": "/image/tj-3.png"
   }],
-  index_middle2_img: '/image/tj-1.jpg'
+  "index_middle2_img": "/image/tj-1.jpg"
   
 }
