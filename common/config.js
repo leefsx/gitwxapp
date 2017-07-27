30d6c5e1e98990238ebee90cf7d4d95608ae84b5
leefsx <leefsx@aliyun.com>
2017-07-27 21:49:31 +0800

    config

--- common/config.js
+++ common/config.js
@@ -1,21 +1,14 @@
-
 module.exports = {
-  "domain": "https://lishifeng2.mywopop.com",
-  "website_name": "某某零食",
-  "index_autoplay_img": [
-    "/image/bg-1.jpg",
-    "/image/bg-2.jpg"
-  ],
-  "index_middle_img": [
-  {
-      "url":"details/details",
-      "src": "/image/tj-2.png"
-  },
-  {
-      "url": "details/details",
-      "src": "/image/tj-3.png"
-  }],
-  "index_middle2_img": "/image/tj-1.jpg",
-  "APITOKEN": "Bpp9Sbx5rm9Y5zzQBbXGWS8h0i74kSJf",
-  "appid": "wx24b2b2feed56da32"
-}
+  "domain":"https://lishifeng2.mywopop.com",
+  "website_name":"建站商城",
+  "index_autoplay_img":[
+  "http://lishifeng2.mywopop.com/template/default/images/wxapplet/style1/swiper/bg-1.jpg",
+  "http://lishifeng2.mywopop.com/template/default/images/wxapplet/style1/swiper/bg-2.jpg"],
+  "index_middle_img":["http://lishifeng2.mywopop.com/template/default/images/wxapplet/style1/adspic/tj-2.png","http://lishifeng2.mywopop.com/template/default/images/wxapplet/style1/adspic/tj-3.png"],
+  "index_middle2_img":["http://lishifeng2.mywopop.com/template/default/images/wxapplet/style1/banner/tj-1.jpg"],
+  "appid":"wx24b2b2feed56da32",
+  "wxpaykey":"what147are258the369API000Partner",
+  "apitoken":"Bpp9Sbx5rm9Y5zzQBbXGWS8h0i74kSJf",
+  "appsecret":"5d5c052719093405f6df5e4ae9684254","mch_id":"1248985201",
+  "logo":["http://lishifeng2.mywopop.com/template/default/images/wxapplet/style1/logo/nav.png"]
+  };
